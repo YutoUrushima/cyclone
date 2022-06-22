7acd996a1edf32e50f26745a618aa404c05d82b5
YutoUrushima <yuto259233@gmail.com>
2022-06-22 19:39:12 +0900

    Fix the sending message logic

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,26 +10,33 @@ import (
 )
 
 type RequestBody struct {
-	Channel string
-	Text    string
+	channel string
+	text    string
 }
 
 func main() {
 	godotenv.Load()
-	requestBody := &RequestBody{
-		Channel: os.Getenv("CHANNEL_ID"),
-		Text:    "Hello world",
-	}
-	jsonString, err := json.Marshal(requestBody)
-	if err != nil {
-		panic("Error")
-	}
-	req, err := http.NewRequest("POST", os.Getenv("SLACK_URL"), bytes.NewBuffer(jsonString))
+	// requestBody := &RequestBody{
+	// 	channel: os.Getenv("CHANNEL_ID"),
+	// 	text:    "Hello world",
+	// }
+	// jsonString, err := json.Marshal(requestBody)
+	// if err != nil {
+	// 	panic("Error")
+	// }
+	req, err := http.NewRequest("POST", os.Getenv("SLACK_URL"), nil)
 	if err != nil {
 		panic(("Error"))
 	}
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Authorization", "Bearer "+os.Getenv("BEARER_TOKEN"))
+
+	params := req.URL.Query()
+	params.Add("channel", os.Getenv("CHANNEL_ID"))
+	params.Add("text", "Hello,world2")
+	req.URL.RawQuery = params.Encode()
+
+	fmt.Printf("request -> %v\n", req)
 	client := new(http.Client)
 	res, err := client.Do(req)
 	if err != nil {
