98f94626b0727720720733b4e2d0f15c689cf283
YutoUrushima <yuto259233@gmail.com>
2022-06-25 19:29:01 +0900

    Adjust json

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 type EventPayload struct {
+	Action     string        `json:"action"`
 	Ref        string        `json:"ref"`
 	Before     string        `json:"before"`
 	After      string        `json:"after"`
@@ -42,6 +43,20 @@ type EventPayload struct {
 		Removed  []interface{} `json:"removed"`
 		Modified []interface{} `json:"modified"`
 	} `json:"head_commit"`
+	Organization struct {
+		Login            string `json:"login"`
+		ID               int    `json:"id"`
+		NodeID           string `json:"node_id"`
+		URL              string `json:"url"`
+		ReposURL         string `json:"repos_url"`
+		EventsURL        string `json:"events_url"`
+		HooksURL         string `json:"hooks_url"`
+		IssuesURL        string `json:"issues_url"`
+		MembersURL       string `json:"members_url"`
+		PublicMembersURL string `json:"public_members_url"`
+		AvatarURL        string `json:"avatar_url"`
+		Description      string `json:"description"`
+	} `json:"organization"`
 	Repository struct {
 		ID       int    `json:"id"`
 		NodeID   string `json:"node_id"`
@@ -164,6 +179,45 @@ type EventPayload struct {
 		Type              string `json:"type"`
 		SiteAdmin         bool   `json:"site_admin"`
 	} `json:"sender"`
+	Installation struct {
+		ID      int `json:"id"`
+		Account struct {
+			Login             string `json:"login"`
+			ID                int    `json:"id"`
+			NodeID            string `json:"node_id"`
+			AvatarURL         string `json:"avatar_url"`
+			GravatarID        string `json:"gravatar_id"`
+			URL               string `json:"url"`
+			HTMLURL           string `json:"html_url"`
+			FollowersURL      string `json:"followers_url"`
+			FollowingURL      string `json:"following_url"`
+			GistsURL          string `json:"gists_url"`
+			StarredURL        string `json:"starred_url"`
+			SubscriptionsURL  string `json:"subscriptions_url"`
+			OrganizationsURL  string `json:"organizations_url"`
+			ReposURL          string `json:"repos_url"`
+			EventsURL         string `json:"events_url"`
+			ReceivedEventsURL string `json:"received_events_url"`
+			Type              string `json:"type"`
+			SiteAdmin         bool   `json:"site_admin"`
+		} `json:"account"`
+		RepositorySelection string `json:"repository_selection"`
+		AccessTokensURL     string `json:"access_tokens_url"`
+		RepositoriesURL     string `json:"repositories_url"`
+		HTMLURL             string `json:"html_url"`
+		AppID               int    `json:"app_id"`
+		TargetID            int    `json:"target_id"`
+		TargetType          string `json:"target_type"`
+		Permissions         struct {
+			Metadata string `json:"metadata"`
+			Contents string `json:"contents"`
+			Issues   string `json:"issues"`
+		} `json:"permissions"`
+		Events         []string `json:"events"`
+		CreatedAt      int      `json:"created_at"`
+		UpdatedAt      int      `json:"updated_at"`
+		SingleFileName string   `json:"single_file_name"`
+	} `json:"installation"`
 }
 
 func HandleLambdaEvent(eventPayload EventPayload) {
@@ -185,7 +239,7 @@ func HandleLambdaEvent(eventPayload EventPayload) {
 
 	params := req.URL.Query()
 	params.Add("channel", os.Getenv("CHANNEL_ID"))
-	params.Add("text", "Info: "+eventPayload.Ref)
+	params.Add("text", eventPayload.Action)
 	req.URL.RawQuery = params.Encode()
 
 	fmt.Printf("request -> %v\n", req)
