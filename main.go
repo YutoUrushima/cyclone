8ce21d9fb3bb616d25eddca59436af6ae94f8d9b
YutoUrushima <yuto259233@gmail.com>
2022-06-25 19:36:01 +0900

    Adjust json 3

--- main.go
+++ main.go
@@ -237,7 +237,7 @@ func HandleLambdaEvent(eventPayload EventPayload) {
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Authorization", "Bearer "+os.Getenv("BEARER_TOKEN"))
 
-	fmt.Println(eventPayload)
+	fmt.Printf("data -> %v\n", eventPayload)
 
 	params := req.URL.Query()
 	params.Add("channel", os.Getenv("CHANNEL_ID"))
